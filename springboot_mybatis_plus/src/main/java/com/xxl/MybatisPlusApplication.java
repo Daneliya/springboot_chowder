be8d02f04db2482295d874f2c07e91deabcd0cc7
xu_xiaolong <luckily126163@126.com>
2022-08-20 15:42:35 +0800

    springboot炖mybatis-plus

---
+++ springboot_mybatis_plus/src/main/java/com/xxl/MybatisPlusApplication.java
@@ -0,0 +1,15 @@
+package com.xxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xxl
+ * @Date: 2022/08/20 15:36
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+}
