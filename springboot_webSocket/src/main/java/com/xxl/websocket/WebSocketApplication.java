33358970fb59c0b6db44e0850721b7b391a514d5
xu_xiaolong <luckily126163@126.com>
2023-04-20 11:44:46 +0800

    springboot炖webSocket代码提交

---
+++ springboot_webSocket/src/main/java/com/xxl/websocket/WebSocketApplication.java
@@ -0,0 +1,23 @@
+package com.xxl.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
