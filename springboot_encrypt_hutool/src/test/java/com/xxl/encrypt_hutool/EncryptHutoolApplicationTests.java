265704adbf9d7ad962e51bc2894c273b16a29cf6
xu_xiaolong <luckily126163@126.com>
2023-06-02 17:04:29 +0800

    代码提交

--- springboot_encrypt_hutool/src/test/java/com/xxl/encrypt_hutool/EncryptHutoolApplicationTests.java
+++ springboot_encrypt_hutool/src/test/java/com/xxl/encrypt_hutool/EncryptHutoolApplicationTests.java
@@ -1,13 +1,8 @@
 package com.xxl.encrypt_hutool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EncryptHutoolApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
