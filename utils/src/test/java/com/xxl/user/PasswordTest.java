220c360e8853de38e6cba673c84a5bd98cd9e877
xu_xiaolong <luckily126163@126.com>
2023-07-06 14:35:52 +0800

    代码提交

---
+++ utils/src/test/java/com/xxl/user/PasswordTest.java
@@ -0,0 +1,25 @@
+package com.xxl.user;
+
+import org.junit.Test;
+
+/**
+ * @Description: 密码加密测试
+ * @Author: xxl
+ * @Date: 2023/07/06 10:23
+ * @Version: 1.0
+ */
+public class PasswordTest {
+
+    /**
+     * 生成
+     */
+    @Test
+    public void test01() {
+        String mobile = "13953991239";
+        String salt = PasswordUtils.getSalt();
+        //生成密码
+        String password = PasswordUtils.encode(mobile.substring(5, 11), salt);
+        System.out.println("salt: " + salt);
+        System.out.println("password: " + password);
+    }
+}
