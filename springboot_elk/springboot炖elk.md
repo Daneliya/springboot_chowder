79c7483461fa2c08dac99a31112a87009d685be0
Daneliya <luckily126163@126.com>
2023-11-17 00:45:00 +0800

    代码提交

--- springboot_elk/springboot炖elk.md
+++
@@ -1,7 +0,0 @@
-
-
-https://www.zhihu.com/tardis/zm/art/347378314?source_id=1002
-
-https://blog.csdn.net/m0_51510236/article/details/130413227
-
-https://blog.csdn.net/captain_zkk/article/details/124799087
--- springboot_elk/src/main/java/com/xxl/elk/controller/ElkController.java
+++ springboot_elk/src/main/java/com/xxl/elk/controller/ElkController.java
@@ -1,23 +1,28 @@
 package com.xxl.elk.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xxl
  * @date 2023/11/16
  */
+@Slf4j
 @RequestMapping("/api")
 @RestController
 public class ElkController {
 
-    @GetMapping
-    public String addLog(@RequestParam(value = "param1", required = false) String param1) {
-        return "你好，这段话将被日志记录";
+    /**
+     * 测试输出log的访问方法
+     */
+    @GetMapping("/testLog")
+    public String testLog() {
+        log.info("{日志},{}", "测试输出一个日志");
+        log.error("{日志},{}", "测试输出一个错误日志");
+        return "success";
     }
-
 
 }
 
