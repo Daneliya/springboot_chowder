a27a94da35e552e6b584a50c4ddbab0c31657e2a
Daneliya <luckily126163@126.com>
2024-09-09 01:05:31 +0800

    pdf代码提交

---
+++ springboot_pdf/src/main/java/com/xxl/springboot_pdf/SpringbootPdfApplication.java
@@ -0,0 +1,16 @@
+package com.xxl.springboot_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class SpringbootPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPdfApplication.class, args);
+    }
+
+}
