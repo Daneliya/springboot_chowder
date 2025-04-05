53786432be9d92458aaa262ea806f48124dd6236
Daneliya <luckily126163@126.com>
2025-04-05 21:07:06 +0800

    springboot炖ai 代码提交

---
+++ springboot_ai_alibaba/src/main/java/com/xxl/ai/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.xxl.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname ChatController
+ * @Description TODO
+ * @Date 2025/4/4 23:38
+ * @Created by xxl
+ */
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(value = "input") String input) {
+        return this.chatClient.prompt()
+                .user(input)
+                .call()
+                .content();
+    }
+}
