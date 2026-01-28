68d302967ba6346a3363dcc72d725734893894bf
xu_xiaolong <luckily126163@126.com>
2026-01-28 17:43:45 +0800

    feat: 代码提交

---
+++ springboot-docker-compose/src/main/java/com/xxl/docker/compose/DockerComposeApplication.java
@@ -0,0 +1,18 @@
+package com.xxl.docker.compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author xxl
+ * @date 2026/1/28 09:03
+ */
+@SpringBootApplication
+public class DockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeApplication.class, args);
+    }
+}
---
+++ springboot_swagger3/src/main/java/com/xxl/Main.java
@@ -0,0 +1,11 @@
+package com.xxl;
+
+/**
+ * @Author: xxl
+ * @Date: 2025/3/20 15:48
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
